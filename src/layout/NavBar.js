922bb74e5ccee392ad6cd9ae09dd98cf50052216
Object-ions <moshikoatia@gmail.com>
2024-02-08 12:54:02 -0800

    scuffle app using create-react-app

---
+++ src/layout/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <img src="#" alt="logo" />
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
