e14b045852748655fdb15ab40b4544bed76abb46
Object-ions <moshikoatia@gmail.com>
2024-02-10 19:32:29 -0800

    break to smaller components

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,38 +1,21 @@
 import React, { useRef } from 'react';
 import './Home.scss';
 import homeData from './home-data.json';
-import ParallaxImage from '../../components/ParallaxImage';
-import { useParallaxEffect } from '../../hooks/useParallaxEffect';
+import HomeImages from './HomeImages';
 
 const Home = () => {
-  const numberOfImages = 13;
-  const imagePath = './venus/';
-  const imageExtension = '.png';
-
-  const images = Array.from(
-    { length: numberOfImages },
-    (_, index) => `${imagePath}${index + 2}${imageExtension}`
-  );
-
-  const homeRef = useRef();
-  useParallaxEffect(homeRef);
-
   return (
     <div className="Home">
       <div className="head-text">
         <h1>Birth of Venus</h1>
         <h4>Sandro Botticelli (Firenze 1445 – 1510)</h4>
+        <p>{homeData.description[0]}</p>
+        <p>{homeData.description[1]}</p>
+        <p>{homeData.description[2]}</p>
       </div>
-      <div className="home-images" ref={homeRef}>
-        {images.map((src, index) => (
-          <ParallaxImage key={index} src={src} index={index} />
-        ))}
-        <img className="static-image" src="./venus/1.png" alt="static-bg" />
-      </div>
+      <HomeImages />
       <div className="disclaimer">
-        {homeData.disclaimer.map((paragraph, index) => (
-          <p key={index}>{paragraph}</p>
-        ))}
+        <p>{homeData.disclaimer[0]}</p>
       </div>
     </div>
   );
---
+++ src/pages/home/HomeImages.js
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import ParallaxImage from '../../components/ParallaxImage';
+import { useParallaxEffect } from '../../hooks/useParallaxEffect';
+
+const HomeImages = () => {
+  const numberOfImages = 11;
+  const imagePath = './venus/';
+  const imageExtension = '.png';
+
+  const images = Array.from(
+    { length: numberOfImages },
+    (_, index) => `${imagePath}${index + 3}${imageExtension}`
+  );
+
+  const homeRef = useRef();
+  useParallaxEffect(homeRef);
+  return (
+    <div className="home-images" ref={homeRef}>
+      {images.map((src, index) => (
+        <ParallaxImage key={index} src={src} index={index} />
+      ))}
+      <img className="static-image" src="./venus/1.png" alt="static-bg" />
+    </div>
+  );
+};
+
+export default HomeImages;
