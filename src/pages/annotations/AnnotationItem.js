99b1485fbdc214bbbaeb516cc9e40831f25df1e8
Object-ions <moshikoatia@gmail.com>
2024-02-12 17:04:14 -0800

    add annotations data and components

---
+++ src/pages/annotations/AnnotationItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AnnotationItem = () => {
+  return <div>AnnotationItem</div>;
+};
+
+export default AnnotationItem;
--- src/pages/annotations/Annotations.js
+++ src/pages/annotations/Annotations.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import './Annotations.scss';
+import data from './annotations.json';
+import text from './annotations-text.json';
+import AnnotationItem from './AnnotationItem';
 
-const Annotations = () => {
-  return <div>Annotations</div>;
+const fresco = data[0];
+const engraving = data[1];
+const statue = data[2];
+
+const Annotations = ({ fresco, engraving, statue }) => {
+  return (
+    <div className="Annotations">
+      <div className="annotations-intro">
+        <h1>{text[0].title}</h1>
+        <h2>{text[0].subtitle}</h2>
+        <p>By: {text[0].by}</p>
+        <p>{text[0].introduction}</p>
+      </div>
+      <div className="annotations-body">
+        <AnnotationItem {...fresco} />
+        <AnnotationItem {...engraving} />
+        <AnnotationItem {...statue} />
+      </div>
+      <div className="annotations-conclusion">
+        <p>{text[0].conclusion}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Annotations;
